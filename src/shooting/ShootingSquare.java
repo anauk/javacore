bb650c7f8a4c06b181584213ba6e3fadf52dc081
Marina <anauk2018@gmail.com>
2018-11-16 10:32:13 +0200

    task1end

--- src/shooting/ShootingSquare.java
+++ src/shooting/ShootingSquare.java
@@ -6,37 +6,45 @@ import java.util.Scanner;
 public class ShootingSquare {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[][] square = new int[5][5];
-
-        for (int i = 0; i < square.length; i++) {
-            for (int j = 0; j < square[i].length; j++) {
+        int size = 5;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 System.out.print("- " + "\t");
             }
             System.out.println();
         }
-        int[] squ = new int[5];
-        System.out.println(getRandom(squ));
         System.out.println("All set. Get ready to rumble!");
-
-
+        int[] arrRow = new int[]{1,2,3,4,5};
+int row = getRandom(arrRow);
+int col = getRandom(arrRow);
+        System.out.println(row);
+        System.out.println(col);
         boolean isPlay = true;
         while (isPlay) {
-            int hit = in.nextInt();
-
-                if (hit == getRandom(squ)) {
-                    for (int aSqu : squ) {
-                        System.out.print("* ");
-                    }
-                    System.out.println("You have won!");
-                    isPlay = false;
-                } else {
-                    System.out.println("Try, try, try!");
-                }
-
+            String s = in.nextLine();
+            if(checkNum(s)) {
+    int hit = Integer.parseInt(s);
+    if (hit == row) {
+            System.out.print("*");
         }
+        System.out.println("You have won!");
+        isPlay = false;
+    } else {
+        System.out.println("Try, try, try!");
+    }
+}
     }
 
-        private static int getRandom ( int[] arr){
+    private static boolean checkNum(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static int getRandom ( int[] arr){
             int rnd = (int) (Math.random()*arr.length);
             return arr[rnd];
         }
