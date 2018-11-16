919dc069d679e55cc5bba2016dd973b8ea701f3c
Marina <anauk2018@gmail.com>
2018-11-16 21:55:43 +0200

    proba on number2

--- src/shooting/ShootingSquareEnd.java
+++ src/shooting/ShootingSquareEnd.java
@@ -2,32 +2,19 @@ package shooting;
 
 import java.util.Scanner;
 
-public class ShootingSquare {
+public class ShootingSquareEnd {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         int[][] arr1 = new int[5][5];
-        int x=1;
+        int x = 1;
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr1[i].length; j++) {
-                arr1[i][j]=x++;
+                arr1[i][j] = x++;
             }
-            System.out.println();
         }
-
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr1[i].length; j++) {
-                System.out.print(arr1[i][j]+" ");
-            }
-            System.out.println();
-        }
-
         System.out.println("All set. Get ready to rumble!");
         int countTry = 0;
-        //RandomCells theRandomCells = new RandomCells();
         int rand = (int)(Math.random()*26);
-        //int[] arr = {rand, rand+1};
-        //theRandomCells.setLocCells(arr);
         System.out.println(rand);
         boolean isPlay = true;
         while (isPlay) {
@@ -35,38 +22,32 @@ public class ShootingSquare {
             if(isNumber(s1)) {
                 int s = Integer.parseInt(s1);
                 int result = 0;
-
                 for (int i = 0; i < arr1.length; i++) {
                     for (int j = 0; j < arr1[i].length; j++) {
                         if (s == rand && s == arr1[i][j]) {
                             arr1[i][j] = result;
                             System.out.printf("You won with %d hit!", countTry);
+                            System.out.println();
                             isPlay = false;
                         }
                         if (s != rand && s == arr1[i][j]) {
                             arr1[i][j] = 111;
                         }
                     }
-                    System.out.println();
                 }
-                for (int i = 0; i < arr1.length; i++) {
-                    for (int j = 0; j < arr1[i].length; j++) {
-                        System.out.print(arr1[i][j] + "  ");
-                    }
-                    System.out.println();
-                }
-                //String result = theRandomCells.getHit(s);
+                printSquare(arr1);
                 countTry++;
-
             }
-
-      /* if(result.equals("x")) {
-           isPlay=false;
-           System.out.printf("You did %s trys!", countTry );
-       }*/
-        //System.out.println("Try, try, try!");
+        }
     }
 
+    private static void printSquare(int[][] arr1) {
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                System.out.print(arr1[i][j] + "  ");
+            }
+            System.out.println();
+        }
     }
 
     private static boolean isNumber(String s) {
@@ -77,11 +58,5 @@ public class ShootingSquare {
         }
         return true;
     }
-
-
-    private static int getRandom ( int[] arr){
-            int rnd = (int) (Math.random()*arr.length);
-            return arr[rnd];
-        }
 
 }
