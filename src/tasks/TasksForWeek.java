86c2a07934f29407f60ccfd423ef553923d7fd50
Marina <anauk2018@gmail.com>
2018-11-20 20:03:29 +0200

    homwork 3

--- src/tasks/TasksForWeek.java
+++ src/tasks/TasksForWeek.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TasksForWeek{
@@ -19,15 +20,39 @@ public class TasksForWeek{
         scedule[5][1] = "meet with parents";
         scedule[6][0] = "saturday";
         scedule[6][1] = "bla bla bla";
-
-        System.out.println("Please, input the day of the week:");
         Scanner in = new Scanner(System.in);
-        String s = in.next().toUpperCase();
-            getDoTask(scedule, s);
+        System.out.println("Please, input the day of the week:");
+        while(in.hasNextLine()) {
+            String s = in.nextLine();int index = s.indexOf("change");
+            if(index == -1){
+                if(s.length() == 0 ) {break;}
+                getDoTask(scedule, s);
+                System.out.println("Please, input the day of the week again:");
+            } else {
+                scedule = new String[scedule.length][scedule[0].length];
+                for (int i = 0; i < scedule.length; i++) {
+                    for (int j = 0; j < scedule[i].length; j++) {
+                        if(s.length() == 0 ) {break;}
+                        System.out.print("Input your day " + i + " and task on this day: " + j+ ":");
+                        scedule[i][j] = in.nextLine();
+                        }
+                }
+                for (String[] aScedule : scedule) {
+                    for (String anAScedule : aScedule) {
+                        System.out.print(anAScedule + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            }
+        }
+        System.out.println("Bay!");
     }
 
     private static void getDoTask(String[][] scedule, String s) {
+        System.out.println("You entered: " + s);
         switch (s.trim().toLowerCase()) {
+            case "EXIT": break;
             case "sunday":
                 System.out.println("Your tasks for Sunday: " + scedule[0][1]);
                 break;
