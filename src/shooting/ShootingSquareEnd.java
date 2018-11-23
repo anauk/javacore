52e2965d97d14deadbf0516fceb5d7421a658c4c
Marina <anauk2018@gmail.com>
2018-11-23 17:14:19 +0200

    homwork 2

--- src/shooting/ShootingSquareEnd.java
+++ src/shooting/ShootingSquareEnd.java
@@ -9,8 +9,8 @@ public class ShootingSquareEnd {
         String[][] arr1 = getStringsArray();
         System.out.println("All set. Get ready to rumble!");
         int countTry = 0;
-        int row = (int)(Math.random()*5);
-        int coll = (int)(Math.random()*5);
+        int row = (int)(Math.random()*5)+1;
+        int coll = (int)(Math.random()*5)+1;
         System.out.println(row);
         System.out.println(coll);
         boolean isPlay = true;
@@ -20,20 +20,26 @@ public class ShootingSquareEnd {
             if(isNumber(s1)&&isNumber(s2)) {
                 int n1 = Integer.parseInt(s1);
                 int n2 = Integer.parseInt(s2);
+                countTry++;
                     if (n1 == row && n2 == coll) {
                         arr1[n1][n2] = "x";
                         System.out.printf("You won with %d hit!", countTry);
                         System.out.println();
                         isPlay = false;
                     } else {
+
                         try {
+                            if((n1==0||n1>5) &&(n2==0||n2>5)){
+                                System.out.println("Try again!");
+                            }
                             arr1[n1][n2] = "*";
+
                         } catch (ArrayIndexOutOfBoundsException e) {
-                            System.out.println("Enter number<5");
+                            System.out.println("Enter number with 1 to 5");
                         }
                     }
                     printSquare(arr1);
-                    countTry++;
+
             }
         }
     }
@@ -44,15 +50,18 @@ public class ShootingSquareEnd {
         System.out.println("All set. Get ready to rumble!");
         int countTry = 0;
         int numOfHits=0;
-        int rand = (int)(Math.random()*5);
-        int[] randShip = {rand, rand+1, rand+2};
+        int row = (int)(Math.random()*arr1.length);
+        int col = (int)(Math.random()*arr1[0].length);
+        int[][] randShip = new int[3][1];
+        randShip[row][col]=1;
+        randShip[row+1][col]=1;
+        randShip[row+2][col]=1;
         /*int coll = (int)(Math.random()*5);
         int[] randColl = {coll, coll+1, coll+2};*/
-        System.out.println(rand);
         /*System.out.println(coll);*/
-        System.out.println(Arrays.toString(randShip));
+        System.out.println(Arrays.deepToString(randShip));
         /*System.out.println(Arrays.toString(randColl));*/
-        while(countTry<3) {
+        /*while(countTry<3) {
             String s = in.nextLine();
             int n1 = Integer.parseInt(s);
             String result;
@@ -68,27 +77,33 @@ public class ShootingSquareEnd {
             }
                     numOfHits++;
             System.out.println("You did it!");
-        }
-        /*boolean isPlay = true;
+        }*/
+        boolean isPlay = true;
         while (isPlay) {
             String s1 = in.nextLine();
             String s2 = in.nextLine();
             if(isNumber(s1)&&isNumber(s2)) {
                 int n1 = Integer.parseInt(s1);
                 int n2 = Integer.parseInt(s2);
-                if (n1 == rand && n2 == coll) {
-                    arr1[n1][n2] = "x";
-                    System.out.printf("You won with %d hit!", countTry);
-                    System.out.println();
-                    isPlay = false;
-                } else {
-                    arr1[n1][n2] = "*";
+                for (row = 0; row < randShip.length; row++) {
+                    for (col = 0; col < randShip[row].length; col++) {
+                        if (n1 == row && n2 == col) {
+                            arr1[n1][n2] = "x";
+                            numOfHits++;
+                            System.out.printf("You won with %d hit!", countTry);
+                            System.out.println();
+                            isPlay = false;
+                        } else if ((n1 == row && n2 != col) || (n1 != row && n2 == col)) {
+                            arr1[n1][n2] = "*";
+                        } else {
+                            arr1[n1][n2] = "-";
+                        }
+                    }
                 }
-
                 printSquare(arr1);
                 countTry++;
             }
-        }*/
+        }
     }
     public String checkHit(String s, int[] rand) {
         int numOfHits=0;
@@ -112,9 +127,9 @@ public class ShootingSquareEnd {
         return result;
     }
     private static String[][] getStringsArray() {
-        String[][] arr1 = new String [5][5];
+        String[][] arr1 = new String [6][6];
         for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr1[i].length; j++) {
+            for (int j = 1; j < arr1[i].length; j++) {
                 arr1[i][j] = "-";
             }
         }
@@ -123,13 +138,17 @@ public class ShootingSquareEnd {
 
     private static void printSquare(String[][] arr1) {
         int row = 0;
-            for (int i = 0; i < arr1.length; i++) {
-                System.out.print(row++ +"| ");
-                for (int j = 0; j < arr1[i].length; j++) {
-                    System.out.print(arr1[i][j] + " | ");
-                }
-                System.out.println();
+        int num = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(row++ +"| ");
+            for (int j = 1; j < arr1[i].length; j++) {
+                if(i == 0){
+                    System.out.print(++num +" | ");
+                } else{
+                    System.out.print(arr1[i][j] + " | ");}
             }
+            System.out.println();
+        }
     }
 
     private static boolean isNumber(String s) {
